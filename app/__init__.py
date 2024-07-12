11e4bb7d35b3c2d0c8b3cb70e1f6662462eedf55
Misheck Gogo <misheckgogo45@gmail.com>
2024-07-12 13:05:35 -0700

    Initialize Flask app with SQLAlchemy, Migrate, and LoginManager update

--- app/__init__.py
+++ app/__init__.py
@@ -5,25 +5,17 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
+login = LoginManager()
+login.login_view = 'login'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cointrack.db'
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    
+    app.config.from_object('config.Config')
+
     db.init_app(app)
     migrate.init_app(app, db)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login.init_app(app)
 
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    from .routes import main
-    app.register_blueprint(main)
-
-    return app
+    with app.app_context():
+        from . import routes, models
+        return app
