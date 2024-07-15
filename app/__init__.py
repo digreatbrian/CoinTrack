15302f189285aa0ae1fa617ff9653424d39e88cc
Misheck Gogo <misheckgogo45@gmail.com>
2024-07-15 09:11:37 -0700

    updating Initialize Flask app with SQLAlchemy, Migrate, and LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,34 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-login = LoginManager()
-login.login_view = 'login'
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
 
+    # Configuration
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize plugins
     db.init_app(app)
     migrate.init_app(app, db)
-    login.init_app(app)
+    login_manager.init_app(app)
 
-    with app.app_context():
-        from . import routes, models
-        return app
+    # Set login view for login_manager
+    login_manager.login_view = 'auth.login'
+
+    # Import Blueprints
+    from .main.routes import main as main_blueprint
+    from .auth.routes import auth as auth_blueprint
+    from .income.routes import income as income_blueprint
+    from .expenses.routes import expenses as expenses_blueprint
+
+    # Register Blueprints
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(income_blueprint)
+    app.register_blueprint(expenses_blueprint)
+
+    return app
